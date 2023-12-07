de6c603e59f6789da36a78d9eb394acf315181d8
Nastya <anastasiia.havrylchyk@oa.edu.ua>
2023-12-07 20:16:09 +0200

    lab 8

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -2,23 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts;
+using System;
+using System.IO;
 
 namespace Assets.Scripts
 {
+    [System.Serializable]
+    public class PlayerData
+    {
+        public string Name;
+        public int cats;
+    }
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private int Cats;
+        public Action<int> OnCatsAmountcanged;
 
-        [SerializeField] private int cats;
+        private void Start()
+        {
+            var path = Application.persistentDataPath + "/playerData.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+
+                var playerData = JsonUtility.FromJson<PlayerData>(json);
+
+                Cats = playerData.cats;
+            }
+            else
+                Cats = 0;
+
+            OnCatsAmountcanged?.Invoke(Cats);
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.TryGetComponent<Catch>(out var cat)) 
+            if (collision.TryGetComponent<Catch>(out var cat))
             {
                 var catValue = cat.Collect();
-                cats += catValue;
+                Cats += catValue;
+                var json = JsonUtility.ToJson(new PlayerData() { Name = "Wizard", cats = Cats });
+                //Debug.Log(json);
+
+                var path = Application.persistentDataPath + "/playerData.json";
+                File.WriteAllText(path, json);
+
+                OnCatsAmountcanged?.Invoke(Cats);
             }
         }
 
     }
-  
+
 }
